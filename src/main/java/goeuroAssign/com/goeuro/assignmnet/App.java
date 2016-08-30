40ee4b99b4dd43c36dd73907d1ebd8321f7163e3
shebadh <sherif.ebadh@gmail.com>
2016-08-30 15:57:07 +0200

    Add FilePath handling Add outPut Sample

--- src/main/java/goeuroAssign/com/goeuro/assignmnet/App.java
+++ src/main/java/goeuroAssign/com/goeuro/assignmnet/App.java
@@ -12,20 +12,25 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String filePath = "c:\\test.csv" ;
+    	String filePath = "output.csv" ;
     	if(args.length <= 0){
     		System.out.println("CityName must be passed as Argument");
        }else{
-    	   String result = ServiceUtility.callServiceAPI(args[0]);
-    	   if(result != null){
-    		   JSONArray infoArray  = ServiceUtility.prepareJSONArray(result);
-    		   if(ServiceUtility.createCSVFile(infoArray , filePath)){
-        		   System.out.println("File Created succesfully on the following path "+filePath);
+    	  if(args.length == 2 && args[1] != null){
+    		  // this to change the output path file - otional parameter -
+    		  filePath = args[1];     		  
+    	  }
+    	  if(args[0] != null){
+    		   String result = ServiceUtility.callServiceAPI(args[0]);
+        	   if(result != null){
+        		   JSONArray infoArray  = ServiceUtility.prepareJSONArray(result);
+        		   if(ServiceUtility.createCSVFile(infoArray , filePath)){
+            		   System.out.println("File Created succesfully on the following path "+filePath);
+            	   }
         	   }
     	   }
     	   
        }
-    	
         
     }
 }
